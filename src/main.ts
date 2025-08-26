b5e72bcf057bd3f0c7b04153647fe4af6cb3a951
annsabbasi <annsabbasi54@gmail.com>
2025-08-26 10:41:51 +0500

    change

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
+import { Server } from 'http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const expressApp = express();
+let server: Server;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // // Enable CORS
-  // app.enableCors({
-  //   origin: ['http:localhost:3000', 'http:localhost:3001',],
-  //   Credential: true
-  // })
-
-  // app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-
-  await app.listen(process.env.PORT ?? 3020);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  app.enableCors();
+  await app.init();
+  return expressApp;
 }
-bootstrap();
+
+// Vercel serverless handler
+export default async function handler(req: any, res: any) {
+  if (!server) {
+    const expressApp = await bootstrap();
+    server = expressApp.listen();
+  }
+  server.emit('request', req, res);
+}
