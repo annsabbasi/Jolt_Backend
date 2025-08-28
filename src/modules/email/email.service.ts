d279d4d12b84b0a755b75c3a41deca93218f9274
annsabbasi <annsabbasi54@gmail.com>
2025-08-28 15:35:04 +0500

    config changes for NodeMailer

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -7,9 +7,9 @@ export class EmailService {
   private transporter: nodemailer.Transporter;
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST || 'smtp.gmail.com',
-      port: parseInt('465'),
-      secure: true,
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
